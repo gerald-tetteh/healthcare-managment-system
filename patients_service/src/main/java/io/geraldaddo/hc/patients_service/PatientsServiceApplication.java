62bdb0dddc9b52fb46022b9f77329c58f3944d95
Gerald Addo-Tetteh <geraldadt@outlook.com>
2025-03-27 14:22:01 +0000

    update configuration imports

--- patients_service/src/main/java/io/geraldaddo/hc/patients_service/PatientsServiceApplication.java
+++ patients_service/src/main/java/io/geraldaddo/hc/patients_service/PatientsServiceApplication.java
@@ -1,12 +1,9 @@
 package io.geraldaddo.hc.patients_service;
 
-import io.geraldaddo.hc.security_module.SecurityModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({SecurityModule.class})
 public class PatientsServiceApplication {
 
     public static void main(String[] args) {
