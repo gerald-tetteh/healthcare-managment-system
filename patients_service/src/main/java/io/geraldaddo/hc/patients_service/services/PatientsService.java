b5125a51f5df7b0b0f5b468468778db645e0a3a1
Gerald Addo-Tetteh <geraldadt@outlook.com>
2025-03-29 01:08:01 +0000

    add patients service tests

--- patients_service/src/main/java/io/geraldaddo/hc/patients_service/services/PatientsService.java
+++ patients_service/src/main/java/io/geraldaddo/hc/patients_service/services/PatientsService.java
@@ -1,5 +1,6 @@
 package io.geraldaddo.hc.patients_service.services;
 
+import io.geraldaddo.hc.patients_service.dtos.UserProfileDto;
 import io.geraldaddo.hc.user_data_module.entities.User;
 import io.geraldaddo.hc.user_data_module.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,27 @@ public class PatientsService {
         User user = getUserById(id);
         user.setActive(false);
         userRepository.save(user);
+    }
+
+    public User updateUser(int id, UserProfileDto userProfileDto) {
+        User user = getUserById(id);
+        user.setFirstName(userProfileDto.getFirstName())
+                .setLastName(userProfileDto.getLastName())
+                .setAge(userProfileDto.getAge())
+                .setNumber(userProfileDto.getNumber())
+                .setEmail(userProfileDto.getEmail())
+                .setDateOfBirth(userProfileDto.getDateOfBirth())
+                .setAddressLineOne(userProfileDto.getAddressLineOne())
+                .setAddressLineTwo(userProfileDto.getAddressLineTwo())
+                .setCity(userProfileDto.getCity())
+                .setCounty(userProfileDto.getCounty())
+                .setCountry(userProfileDto.getCountry())
+                .setEmergencyFirstName(userProfileDto.getEmergencyFirstName())
+                .setEmergencyLastName(userProfileDto.getEmergencyLastName())
+                .setEmergencyNumber(userProfileDto.getEmergencyNumber())
+                .setPostCode(userProfileDto.getPostCode())
+                .setJoined(userProfileDto.getJoined())
+                .setInsuranceNumber(userProfileDto.getInsuranceNumber());
+        return userRepository.save(user);
     }
 }
--- patients_service/src/test/java/io/geraldaddo/hc/patients_service/services/PatientsServiceTest.java
+++ patients_service/src/test/java/io/geraldaddo/hc/patients_service/services/PatientsServiceTest.java
@@ -1,7 +1,10 @@
 package io.geraldaddo.hc.patients_service.services;
 
 import io.geraldaddo.hc.patients_service.configurations.PatientsServiceTestConfiguration;
+import io.geraldaddo.hc.patients_service.dtos.UserProfileDto;
+import io.geraldaddo.hc.user_data_module.entities.User;
 import io.geraldaddo.hc.user_data_module.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,22 +12,61 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @Import(PatientsServiceTestConfiguration.class)
 class PatientsServiceTest {
-
     @Autowired
     PatientsService patientsService;
     @MockitoBean
     UserRepository userRepository;
 
+    @BeforeEach
+    void setUp() {
+        when(userRepository.findById(anyInt()))
+                .thenReturn(Optional.of(new User()
+                        .setUserId(0)
+                        .setActive(true)
+                        .setFirstName("Test")
+                        .setLastName("User")
+                        .setNumber("+443801385938")));
+    }
+
     @Test
     void getUserById() {
+        User user = patientsService.getUserById(0);
+        assertEquals(user.getUserId(), 0);
+        verify(userRepository, times(1)).findById(0);
     }
 
     @Test
     void deactivateUser() {
+        patientsService.deactivateUser(0);
+        verify(userRepository, times(1)).findById(0);
+        verify(userRepository, times(1)).save(new User()
+                .setUserId(0)
+                .setActive(false));
+    }
+
+    @Test
+    void updateUser() {
+        User expectedUser = new User()
+                .setUserId(0)
+                .setFirstName("AlternateTest")
+                .setLastName("User")
+                .setNumber("+443801385938");
+        UserProfileDto dto = new UserProfileDto()
+                .setFirstName("AlternateTest")
+                .setLastName("User")
+                .setNumber("+443801385938");
+        when(userRepository.save(expectedUser)).thenReturn(expectedUser);
+        patientsService.updateUser(0, dto);
+        verify(userRepository, times(1)).findById(0);
+        verify(userRepository, times(1)).save(expectedUser);
     }
 }
