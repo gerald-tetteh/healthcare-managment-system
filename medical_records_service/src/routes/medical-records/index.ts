46dd55ad9fbff06560d11901f496be681ff2a524
Gerald Addo-Tetteh <geraldadt@outlook.com>
2025-04-13 22:52:50 +0100

    add error handling and extra logging in getMedicalRecord

--- medical_records_service/src/routes/medical-records/index.ts
+++ medical_records_service/src/routes/medical-records/index.ts
@@ -1,16 +1,19 @@
-import { FastifyPluginAsync } from 'fastify';
+import { FastifyInstance, FastifyPluginAsync, FastifyReply, FastifyRequest } from 'fastify';
 import { createMedicalRecordSchema, getMedicalRecordSchema } from '../../schemas/schemas';
 import MedicalRecord from '../../models/MedicalRecord';
 
 const medicalRecords: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.post('/', {
     schema: createMedicalRecordSchema,
-    preHandler: [fastify.authenticate, fastify.authorizeByRole(["ROLE_DOCTOR", "ROLE_ADMIN"])] 
+    preHandler: [
+      fastify.authenticate, 
+      fastify.authorizeByRole(["ROLE_DOCTOR", "ROLE_ADMIN"])
+    ] 
   }, async function (request, reply) {
     const record = MedicalRecord.fromJson(request.body);
     const encryptedRecord = fastify.encrypt(record);
     const result = await fastify.mongo.db?.collection('medicalRecords').insertOne(encryptedRecord);
-    fastify.log.info(`Inserted medical record with id: ${result?.insertedId}`);
+    fastify.log.info(`User: ${request.user.userId} inserted medical record with id: ${result?.insertedId}`);
     return {
       status: 'success',
       message: 'Medical record created successfully',
@@ -22,15 +25,40 @@ const medicalRecords: FastifyPluginAsync = async (fastify, opts): Promise<void> 
 
   fastify.get("/:id", {
     schema: getMedicalRecordSchema,
-    preHandler: [fastify.authenticate, fastify.authorizeByRole(["ROLE_DOCTOR", "ROLE_ADMIN"])]
+    preHandler: [
+      fastify.authenticate, 
+      fastify.authorizeByRole(["ROLE_DOCTOR", "ROLE_ADMIN"])
+    ]
   }, async function (request, reply) {
-    const id = (request.params as { id: string }).id;
-    const record = await fastify.mongo.db?.collection('medicalRecords').findOne(
-      { _id: new fastify.mongo.ObjectId(id) }
-    );
+    const record = await getMedicalRecord(fastify, request, reply);
     const decryptedRecord = fastify.decrypt(record);
+    fastify.log.info(`User: ${request.user.userId} fetched medical record with id: ${record?._id}`);
     return decryptedRecord;
   });
+};
+
+const getMedicalRecord = async (fastify: FastifyInstance, request: FastifyRequest, reply: FastifyReply) => {
+  const recordId = (request.params as { id: string }).id;
+  const record = await fastify.mongo.db?.collection('medicalRecords').findOne({ _id: new fastify.mongo.ObjectId(recordId) });
+  if (!record) {
+    reply.status(404).send({ 
+      title: "Record Not Found", 
+      message: "The requested record does not exist", 
+      statusCode: "NOT_FOUND" 
+    });
+    return;
+  }
+  const user = request.user;
+  const isPatient = user && user.roles.some(role => role === "ROLE_PATIENT");
+  if (isPatient && record.patientId !== user.userId) {
+    reply.status(403).send({ 
+      title: "Authorization Failed", 
+      message: "Cannot access this resource", 
+      statusCode: "FORBIDDEN" 
+    });
+    return;
+  }
+  return record;
 };
 
 export default medicalRecords;
